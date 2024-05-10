2263080576db997363b6d3086862497912136eb0
DimaYermolenkoOut <dima.yermolenko@outlook.com>
2024-05-10 17:36:23 +0300

    pokemon

--- pokemon.py
+++ pokemon.py
@@ -1,42 +1,4 @@
-# import aiohttp
-# import asyncio
-#
-# async def fetch_pokemon_data(id):
-#     url = f"https://pokeapi.co/api/v2/pokemon/{id}"
-#     async with aiohttp.ClientSession() as session:
-#         async with session.get(url) as response:
-#             data = await response.json()
-#             return data
-#
-#
-# async def main():
-#     pokemon1_id = 1
-#     pokemon2_id = 2
-#
-#     pokemon1_data = await fetch_pokemon_data(pokemon1_id)
-#     pokemon2_data = await fetch_pokemon_data(pokemon2_id)
-#     # print(pokemon1_data['base_experience'])
-#     # print(pokemon2_data['base_experience'])
-#
-#     # Сравниваем опыт покемонов
-#     if pokemon1_data['base_experience'] > pokemon2_data['base_experience']:
-#         print(f"{pokemon1_data['name']} выше опыта у {pokemon2_data['name']}.")
-#     elif pokemon1_data['base_experience'] < pokemon2_data['base_experience']:
-#         print(f"{pokemon2_data['name']} выше опыта у {pokemon1_data['name']}.")
-#     else:
-#         print(f"{pokemon1_data['name']} и {pokemon2_data['name']} одинакового опыта.")
-#
-#     # Сравниваем данные покемонов
-#     if pokemon1_data['weight'] > pokemon2_data['weight']:
-#         print(f"{pokemon1_data['name']} тяжелее, чем {pokemon2_data['name']}.")
-#     elif pokemon1_data['weight'] < pokemon2_data['weight']:
-#         print(f"{pokemon2_data['name']} тяжелее, чем {pokemon1_data['name']}.")
-#     else:
-#         print(f"{pokemon1_data['name']} и {pokemon2_data['name']} одинакового веса.")
-#
-# if __name__ == "__main__":
-#     loop = asyncio.get_event_loop()
-#     loop.run_until_complete(main())
+
 import aiohttp
 import asyncio
 
